a31202de780a61f597eaa1913b0f34467572f608
PercivalN <percival.ngan@gmail.com>
2020-05-24 03:38:09 -0500

    Connected VC to class and added model

---
+++ Swift Vocabulary/Models/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Lambda_School_Loaner_263 on 5/24/20.
+//  Copyright © 2020 Lambda_School_Loaner_263. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    var word: String
+    var definition: String
+}
