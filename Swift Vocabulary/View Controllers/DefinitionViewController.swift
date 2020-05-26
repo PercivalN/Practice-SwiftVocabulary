db0fc104c5dd5d044c436cd71c7baf021304021c
PercivalN <percival.ngan@gmail.com>
2020-05-26 09:13:53 -0500

    Added Alert Controller

--- Swift Vocabulary/View Controllers/DefinitionViewController.swift
+++ Swift Vocabulary/View Controllers/DefinitionViewController.swift
@@ -15,13 +15,13 @@ class DefinitionViewController: UIViewController {
     
     @IBOutlet weak var wordLabel: UILabel!
     @IBOutlet weak var definitionTextView: UITextView!
-    @IBOutlet weak var addButton: UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
         setViews()
-        printAllFonts()
+        //printAllFonts()
     }
     
     func updateViews() {
@@ -29,16 +29,17 @@ class DefinitionViewController: UIViewController {
         guard let word = vocabWord else { return }
         wordLabel.text = word.word
         definitionTextView.text = word.definition
+    
     }
     
     func setViews() {
         
         wordLabel.textColor = .red
         wordLabel.font = UIFont(name: "BradleyHandITCTT-Bold", size: 35)
-        definitionTextView.font = UIFont(name: "BradleyHandITCTT-Bold", size: 12)
+        definitionTextView.font = UIFont(name: "BradleyHandITCTT-Bold", size: 20)
         
-        addButton.setTitle("Add Word", for: .normal)
-        //self.addButton.backgroundColor = .red
+        
+        
     }
     
     func printAllFonts() {
@@ -47,25 +48,4 @@ class DefinitionViewController: UIViewController {
             print(UIFont.fontNames(forFamilyName: familyName))
         }
     }
-    
-    @IBAction func addButton(_ sender: Any) {
-        let alert = UIAlertController(title: "Alert", message: "This is the messsage", preferredStyle: .alert)
-        
-        let action1 = UIAlertAction(title: "Default", style: .default) { (action:UIAlertAction) in
-            print("You've pressed default");
-        }
-
-//        let action2 = UIAlertAction(title: "Cancel", style: .cancel) { (action:UIAlertAction) in
-//            print("You've pressed cancel");
-//        }
-//
-//        let action3 = UIAlertAction(title: "Destructive", style: .destructive) { (action:UIAlertAction) in
-//            print("You've pressed the destructive");
-//        }
-        
-        alert.addAction(action1)
-        
-        self.present(alert, animated: true, completion: nil)
-    }
-    
 }
--- Swift Vocabulary/View Controllers/WordsTableViewController.swift
+++ Swift Vocabulary/View Controllers/WordsTableViewController.swift
@@ -15,100 +15,140 @@ class WordsTableViewController: UITableViewController {
                                         VocabularyWord(word: "Pear", definition: "The edible fruit, typically rounded but elongated and growing smaller toward the stem, of a tree, Pyrus communis, of the rose family."),
                                         VocabularyWord(word: "Pomegranate", definition: "A chambered, many-seeded, globose fruit, having a tough, usually red rind and surmounted by a crown of calyx lobes, the edible portion consisting of pleasantly acid flesh developed from the outer seed coat."),
                                         VocabularyWord(word: "Strawberry", definition: "The fruit of any stemless plant belonging to the genus Fragaria, of the rose family, consisting of an enlarged fleshy receptacle bearing achenes on its exterior.")]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Swift Vocabulary"
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       
-        return vocabWords.count
-    }
-
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
+    func createNewVocabularyWord(with word: String, and definition: String) {
+        let newWord = VocabularyWord(word: word, definition: definition)
+        vocabWords.append(newWord)
+    }
+    
+    // -- -- -- -- -- -- -- -- -- -- -- -- --
+    // MARK: - Actions
+    
+    @IBAction func addWord(_ sender: UIBarButtonItem) {
+        
+        var wordText: UITextField?
+        var definitionText: UITextField?
+        
+        // Declare Alert Message
+        let alert = UIAlertController(title: "Add New Word", message: "", preferredStyle: .alert)
+        
+        // Create Save button
+        alert.addAction(UIAlertAction(title: "SAVE", style: .default) { (action:UIAlertAction) in
+            print("You've pressed default")
+            
+            if let word = wordText?.text, let definition = definitionText?.text {
+                self.createNewVocabularyWord(with: word, and: definition)
+                self.tableView.reloadData()
+            }
+        })
+        
+        alert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: { (action:UIAlertAction) in
+            print("Cancel button tapped")
+        }))
+        
+        alert.addTextField { (wordTextField) in
+            wordText = wordTextField
+            wordText?.placeholder = "Enter word"
+        }
+        
+        alert.addTextField { (definitionTextField) in
+            definitionText = definitionTextField
+            definitionText?.placeholder = "Enter definition"
+        }
+        
+        self.present(alert, animated: true, completion: nil)
+    }
 
+
+
+
+// MARK: - Table view data source
+
+override func numberOfSections(in tableView: UITableView) -> Int {
+    
+    return 1
+}
+
+override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    return vocabWords.count
+}
+
+
+override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
+    
+    let vocabWord = vocabWords[indexPath.row]
+    
+    cell.textLabel?.text = vocabWord.word
+    
+    return cell
+}
+
+
+/*
+ // Override to support conditional editing of the table view.
+ override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+ // Return false if you do not want the specified item to be editable.
+ return true
+ }
+ */
+
+/*
+ // Override to support editing the table view.
+ override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+ if editingStyle == .delete {
+ // Delete the row from the data source
+ tableView.deleteRows(at: [indexPath], with: .fade)
+ } else if editingStyle == .insert {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
+
+/*
+ // Override to support rearranging the table view.
+ override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+ 
+ }
+ */
+
+/*
+ // Override to support conditional rearranging of the table view.
+ override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+ // Return false if you do not want the item to be re-orderable.
+ return true
+ }
+ */
+
+
+// MARK: - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+    // Check the segue identifier to know which view controller you are segueing to
+    if segue.identifier == "ShowDefinitionSegue" {
+        
+        // Get the new view controller using segue.destination.
+        guard let detailVC = segue.destination as? DefinitionViewController,
+            let indexPath = tableView.indexPathForSelectedRow else { return }
+        
+        // Get the information you want to pass to the new view controller
+        
         let vocabWord = vocabWords[indexPath.row]
         
-        cell.textLabel?.text = vocabWord.word
-
-        return cell
-    }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Pass the selected object to the new view controller.
+        detailVC.vocabWord = vocabWord
         
-        // Check the segue identifier to know which view controller you are segueing to
-        if segue.identifier == "ShowDefinitionSegue" {
         
-            // Get the new view controller using segue.destination.
-            guard let detailVC = segue.destination as? DefinitionViewController,
-                let indexPath = tableView.indexPathForSelectedRow else { return }
-            
-            // Get the information you want to pass to the new view controller
-                
-                let vocabWord = vocabWords[indexPath.row]
-                
-                // Pass the selected object to the new view controller.
-                detailVC.vocabWord = vocabWord
-    
-    
-
-}
+    }
 }
 }
